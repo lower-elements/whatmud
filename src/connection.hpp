9d131dc027894b9f35502b089bee6739c8f5980f
Michael Connor Buchan <mikey@blindcomputing.org>
2024-04-25 14:31:49 +0100

    feat: Connection::send() methods for sending data to clients

--- src/connection.hpp
+++ src/connection.hpp
@@ -1,8 +1,11 @@
 #ifndef WHATMUD_CONNECTION_HPP
 #define WHATMUD_CONNECTION_HPP
 
+#include <cstring>
 #include <stddef.h>
+#include <utility>
 
+#include <fmt/core.h>
 #include <libtelnet.h>
 
 #include "uv/tcp.hpp"
@@ -19,6 +22,16 @@ public:
   telnet_t *getTelnet() { return m_telnet; }
   const telnet_t *getTelnet() const { return m_telnet; }
 
+  void send(const char *buf, std::size_t size) {
+    telnet_send(m_telnet, buf, size);
+  }
+  void send(const char *str) { send(str, std::strlen(str)); }
+  void send(const std::string &str) { send(str.c_str(), str.size()); }
+  void send(std::string_view str) { send(str.data(), str.size()); }
+  template <class... Args> void send(Args &&...args) {
+    send(fmt::format(std::forward(args...)));
+  }
+
 protected:
   void onEvent(telnet_event_t &ev);
   void onEof();
@@ -28,9 +41,9 @@ protected:
 private:
   telnet_t *m_telnet;
 
+  friend void onRead(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf);
   friend void forwardEvent(telnet_t *telnet, telnet_event_t *event,
                            void *user_data);
-  void friend onRead(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf);
 };
 
 } // namespace whatmud
